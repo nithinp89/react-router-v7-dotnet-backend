62919ab36d43ea391734207788db03e7b2374ed7
Nithin Prathapan <nithinp89@gmail.com>
2025-04-21 07:22:55 +0200

    theme switcher

--- apps/frontend/app/utils/theme-sessions.server.tsx
+++ apps/frontend/app/utils/theme-sessions.server.tsx
@@ -2,9 +2,6 @@ import { createThemeSessionResolver } from "remix-themes";
 import { createCookieSessionStorage } from "react-router";
 import { Theme } from "~/constants";
 
-// You can default to 'development' if process.env.NODE_ENV is not set
-const isProduction = process.env.NODE_ENV === "production"
-
 const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: Theme.STORAGE_KEY,
@@ -13,11 +10,9 @@ const sessionStorage = createCookieSessionStorage({
     sameSite: "lax",
     secrets: [Theme.COOKIE_SECRET],
     // Set domain and secure only if in production
-    ...(isProduction
-      ? { domain: process.env.APP_DOMAIN, secure: true }
-      : {}),
+    secure: process.env.NODE_ENV === "production",
     // Make cookie persist for 1 year
-    maxAge: 31536000,
+    maxAge: 60*60*24*365,
   },
 })
 
