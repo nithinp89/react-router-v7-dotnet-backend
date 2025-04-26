7656e77db29c514cc906726eacbfef39f3dfc11f
Nithin Prathapan <nithinp89@gmail.com>
2025-04-26 20:53:03 +0200

    update

--- apps/frontend/app/services/cache/cache.server.ts
+++ apps/frontend/app/services/cache/cache.server.ts
@@ -10,7 +10,7 @@ export class CacheService {
   constructor(options?: RedisOptions) {
     this.redis = new Redis({
       host: process.env.REDIS_HOST ?? "127.0.0.1",
-      port: Number(process.env.REDIS_PORT) ?? 6379,
+      port: Number(process.env.REDIS_PORT ?? 6379),
       // password: process.env.REDIS_PASSWORD, // Uncomment if you use a password
       // db: Number(process.env.REDIS_DB) ?? 0, // Optional: select Redis DB
       ...options,
